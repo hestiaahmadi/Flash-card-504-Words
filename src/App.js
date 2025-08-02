b149e1bcbc1d59c96367188a9de20daffc84111a
Hestia Ahmadi <ahmadi.dev05@gmail.com>
2025-08-02 14:10:43 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,61 +5,73 @@ const words = [
   {
     english: "Abandon",
     persian: "ترک کردن",
+    korean: "버리다",
     id: "1",
   },
   {
     english: "Keen",
     persian: "تیز، مشتاق",
+    korean: "날카로운",
     id: "2",
   },
   {
     english: "Jealous",
     persian: "حسود",
+    korean: "질투하는",
     id: "3",
   },
   {
     english: "Tact",
     persian: "تدبیر، درایت",
+     korean: "재치",
     id: "4",
   },
   {
     english: "Oath",
     persian: "سوگند، دشنام",
+    korean: "맹세",
     id: "5",
   },
   {
     english: "Vacant",
     persian: "خالی، اشغال نشده",
+    korean: "빈",
     id: "6",
   },
   {
     english: "Hardship",
     persian: "بی نوایی، سختی",
+    korean: "고난",
     id: "7",
   },
   {
     english: "Gallant",
     persian: "شجاع، جنتلمن",
+    korean: "용감한",
     id: "8",
   },
   {
     english: "Data",
     persian: "اطلاعات، داده ها",
+    korean: "데이터",
     id: "9",
   },
   {
     english: "Unccustomed",
     persian: "غیر عادی، عادت نداشتن",
+    korean: "예사롭지 않은",
     id: "10",
   },
   {
     english: "Bachelor",
     persian: "مرد مجرد، عزب",
+    korean: "미혼 남자",
     id: "11",
   },
   {
     english: "Qulify",
     persian: "واجد شرایط شدن",
+    korean: "자격을 얻다",
     id: "12",
   },
 ];
@@ -87,9 +99,10 @@ function FlashCards() {
           onClick={() => handleClick(word.id)}
           className={word.id === selectedId ? "selected" : ""}
         >
-          <p>{word.id === selectedId ? word.persian : word.english}</p>
+          <p>{word.id === selectedId ? word.korean : word.english}</p>
         </div>
       ))}
     </div>
   );
 }
+
